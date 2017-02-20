0c03d6d997015ce34b7a503fb2f97ccec2aad950
Anas Khan <anas.khan96@outlook.com>
2017-02-21 02:35:07 +0530

    first attempt

---
+++ soup.go
@@ -0,0 +1,33 @@
+package soup
+
+import (
+	"io"
+	"io/ioutil"
+	"net/http"
+)
+
+func Make(url string) (io.ReadCloser, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	return resp.Body, nil
+}
+
+func Prettify(r io.ReadCloser) (string, error) {
+	bytes, err := ioutil.ReadAll(r)
+	if err != nil {
+		return "", err
+	}
+	str := string(bytes)
+	var mod string
+	for i, c := range str {
+		if c == '>' || c == ';' || str[i+1] == '<' {
+			mod += string(c)
+			mod += "\n"
+			continue
+		}
+		mod += string(c)
+	}
+	return mod, nil
+}
