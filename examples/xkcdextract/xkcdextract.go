d9c3e0eb664fbc8e90cd9655ac8721651957dbdc
splinet <splinet@users.noreply.github.com>
2018-08-16 07:26:44 +0300

    Added for-loop and fixed missing https URL prefix.
    Added "https:" prefix concatenation to create clickable links. Also added for-loop for best experience.

--- examples/xkcdextract/xkcdextract.go
+++ examples/xkcdextract/xkcdextract.go
@@ -7,17 +7,19 @@ import (
 )
 
 func main() {
-	fmt.Println("Enter the xkcd comic number :")
-	var num int
-	fmt.Scanf("%d", &num)
-	url := fmt.Sprintf("https://xkcd.com/%d", num)
-	resp, _ := soup.Get(url)
-	doc := soup.HTMLParse(resp)
-	title := doc.Find("div", "id", "ctitle").Text()
-	fmt.Println("Title of the comic :", title)
-	comicImg := doc.Find("div", "id", "comic").Find("img")
-	fmt.Println("Source of the image :", comicImg.Attrs()["src"])
-	fmt.Println("Underlying text of the image :", comicImg.Attrs()["title"])
+	for true {
+		fmt.Println("Enter the xkcd comic number :")
+		var num int
+		fmt.Scanf("%d", &num)
+		url := fmt.Sprintf("https://xkcd.com/%d", num)
+		resp, _ := soup.Get(url)
+		doc := soup.HTMLParse(resp)
+		title := doc.Find("div", "id", "ctitle").Text()
+		fmt.Println("Title of the comic :", title)
+		comicImg := doc.Find("div", "id", "comic").Find("img")
+		fmt.Println("Source of the image :", "https:"+comicImg.Attrs()["src"])
+		fmt.Println("Underlying text of the image :", comicImg.Attrs()["title"])
+	}
 }
 
 /* --- Console I/O ---
